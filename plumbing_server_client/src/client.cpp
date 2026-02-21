63eb8f923dfa672f1dd765eed96261716978e73f
intrepidity <18435058240@163.com>
2026-02-21 22:19:07 +0800

    add files

---
+++ plumbing_server_client/src/client.cpp
@@ -0,0 +1,43 @@
+/*
+    需求: 
+        编写两个节点实现服务通信，客户端节点需要提交两个整数到服务器
+        服务器需要解析客户端提交的数据，相加后，将结果响应回客户端，
+        客户端再解析
+*/
+#include "ros/ros.h"
+#include "plumbing_server_client/Addints.h"
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL,"");
+
+    // 2.初始化 ROS 节点
+    ros::init(argc,argv,"client");
+    // 3.创建 ROS 句柄
+    ros::NodeHandle nh;
+    // 4.创建 客户端 对象
+    ros::ServiceClient client = nh.serviceClient<plumbing_server_client::Addints>("AddInts");
+    //等待服务启动成功
+    //方式1
+    ros::service::waitForService("AddInts");
+    //方式2
+    // client.waitForExistence();
+    // 5.组织请求数据
+    demo03_server_client::AddInts ai;
+    ai.request.num1 = atoi(argv[1]);
+    ai.request.num2 = atoi(argv[2]);
+    // 6.发送请求,返回 bool 值，标记是否成功
+    bool flag = client.call(ai);
+    // 7.处理响应
+    if (flag)
+    {
+        ROS_INFO("请求正常处理,响应结果:%d",ai.response.sum);
+    }
+    else
+    {
+        ROS_ERROR("请求处理失败....");
+        return 1;
+    }
+
+    return 0;
+}
