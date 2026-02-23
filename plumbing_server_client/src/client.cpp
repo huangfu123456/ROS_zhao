23f8d238529ae2429b956a1fa6cbac0b6c96ae14
intrepidity <18435058240@163.com>
2026-02-23 16:15:07 +0800

    add sevver files

--- plumbing_server_client/src/client.cpp
+++ plumbing_server_client/src/client.cpp
@@ -3,13 +3,26 @@
         编写两个节点实现服务通信，客户端节点需要提交两个整数到服务器
         服务器需要解析客户端提交的数据，相加后，将结果响应回客户端，
         客户端再解析
+
+    实现参数的动态提交：
+        rosrun 000 000 12 34
+
 */
+// 1.包含头文件
 #include "ros/ros.h"
 #include "plumbing_server_client/Addints.h"
 
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL,"");
+
+    // 调用时动态传值,如果通过 launch 的 args 传参，需要传递的参数个数 +3
+    if (argc != 3)
+    // if (argc != 5)//launch 传参(0-文件路径 1传入的参数 2传入的参数 3节点名称 4日志路径)
+    {
+        ROS_ERROR("请提交两个整数");
+        return 1;
+    }
 
     // 2.初始化 ROS 节点
     ros::init(argc,argv,"client");
@@ -23,7 +36,7 @@ int main(int argc, char *argv[])
     //方式2
     // client.waitForExistence();
     // 5.组织请求数据
-    demo03_server_client::AddInts ai;
+    plumbing_server_client::Addints ai;
     ai.request.num1 = atoi(argv[1]);
     ai.request.num2 = atoi(argv[2]);
     // 6.发送请求,返回 bool 值，标记是否成功
